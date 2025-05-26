7f9aadf2ee1338790a47ea6cb3768463d21f1ccb
Ecewk <147719873+Ecewk@users.noreply.github.com>
2025-05-27 00:53:16 +0200

    initial commit, nothing works already

---
+++ app/src/main/java/org/UI/UiStarter.java
@@ -0,0 +1,12 @@
+package org.UI;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class UiStarter extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+
+    }
+}
---
+++ app/src/main/java/org/example/Main.java
@@ -0,0 +1,9 @@
+package org.example;
+
+import main.java.org.UI.UiStarter;
+
+public class Main {
+    public static void main(String[] args) {
+        UiStarter.launch(args);
+    }
+}
